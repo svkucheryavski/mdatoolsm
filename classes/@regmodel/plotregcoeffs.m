5570f9f6bab0e809e85cf7733936a0e97c7899cc
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2018-10-31 13:32:21 +0100

    fixed a small bug related to use response name for selection

--- classes/@regmodel/plotregcoeffs.m
+++ classes/@regmodel/plotregcoeffs.m
@@ -1,4 +1,4 @@
 function plotregcoeffs(obj, varargin)
-   [nresp, ncomp, varargin] = regres.getRegPlotParams(obj.nResp, obj.nComp, {}, varargin{:});
+   [nresp, ncomp, varargin] = regres.getRegPlotParams(obj.nResp, obj.nComp, obj.calres.respNames, varargin{:});
    obj.regcoeffs.plot(nresp, ncomp, varargin{:});
 end
